ee9cc8f7ee930ff4bde5e456a086c2d7ffcfd9fc
Rozan <akhmadrozanaditya@gmail.com>
2024-10-15 21:12:32 +0700

    test2
    try github actions

--- tests/test_logger.cpp
+++ tests/test_logger.cpp
@@ -3,9 +3,9 @@
 int main() {
     Logger logger("log.txt");
 
-    logger.log("This is an info message.");
-    logger.log("This is a warning message.", Logger::WARNING);
-    logger.log("This is an error message.", Logger::ERROR);
+    logger.log("This is an info message bro.");
+    logger.log("This is a warning message bro.", Logger::WARNING);
+    logger.log("This is an error message bro.", Logger::ERROR);
 
     return 0;
 }
